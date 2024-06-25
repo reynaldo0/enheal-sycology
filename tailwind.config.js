0994da6e20e8ee9077456163505865e7c9e24080
reynaldo0 <reynaldooyusellino@gmail.com>
2024-06-25 18:15:48 +0700

    fix : hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purple: '#AB67B1',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
