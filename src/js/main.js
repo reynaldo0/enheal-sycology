947037003f1aeaf60c38c003d89518b945e23619
reynaldo0 <reynaldooyusellino@gmail.com>
2024-07-05 19:55:59 +0700

    new : swiper for section kutipan

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Swiper starts
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+    });
+  });
+// Swiper ends
