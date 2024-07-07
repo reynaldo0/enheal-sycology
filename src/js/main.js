9d70c75f5716ce7a15b2e59aebeb8dfbb53d8520
reynaldo0 <reynaldooyusellino@gmail.com>
2024-07-07 11:01:40 +0700

    fix : swiper js content

--- src/js/main.js
+++ src/js/main.js
@@ -4,34 +4,46 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper(".qoute", {
       loop: true,
       pagination: {
-        el: ".swiper-pagination",
         clickable: true
+      },
+      navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
       },
     });
   });
 // Swiper ends
 
-document.addEventListener('DOMContentLoaded', function () {
-const swiper = new Swiper('.testimoni', {
-  effect: 'coverflow',
-  loop: true,
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 3,
-  autoplay: {
-    delay: 5000,
+  document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.testimoni', {
+    effect: 'coverflow',
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
 
-  },
+    },
 
-  coverflowEffect: {
-    rotate: 0,
-    stretch: -20,
-    depth: 100,
-    modifier: 1.5,
-  },
+    coverflowEffect: {
+      slideShadows: false,
+      rotate: 0,
+      stretch: -20,
+      depth: 100,
+      modifier: 2.5,
+    },
 
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-})});
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    initialSlide: 1,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      }
+    }
+  })});
